7f82bf15d378486626d047698afc3bbbce5690e1
Vitya Shataev <vityashataev@gmail.com>
2022-12-22 13:54:33 +0300

    refactor: add env variable PORT

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const costRoute = require('./routes/cost');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 
+const PORT = process.env.PORT || 8800;
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -37,6 +39,6 @@ app.use('/api', costRoute);
 
 
 // Server starting
-app.listen(8800, () => {
-    console.log('Server is running ')
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
