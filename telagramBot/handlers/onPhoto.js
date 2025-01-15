2732104183c4f187abe8ee076d19ba31de70ce7f
Vitya Shataev <vityashataev@gmail.com>
2025-01-15 13:26:20 +0700

    refactor: onPhoto.js

--- telagramBot/handlers/onPhoto.js
+++ telagramBot/handlers/onPhoto.js
@@ -1,17 +1,4 @@
-const { Telegraf } = require('telegraf');
-const fs = require('fs');
-const path = require('path');
 const {preprocessImage, extractTextFromImage} = require("../utils");
-
-// Путь к директории
-const tempDir = path.resolve(__dirname, './temp');
-
-// Создаем директорию, если её нет
-if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir, { recursive: true });
-}
-
-const bot = new Telegraf('YOUR_TELEGRAM_BOT_TOKEN');
 
 const onPhoto = async (ctx) => {
     const fileId = ctx.message.photo.pop().file_id;
